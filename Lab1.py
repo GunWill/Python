5563c49daadf6dc6fe7495e63b063b89926ce3be
GunWill <gunnar.willis@okstate.edu>
2026-08-21 15:16:06 -0500

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,4 @@
-print("Please enter 5 measurements in Centimeters")
+try:
+    measurement1 = float(input("Enter first Measurement in Centimeters"))
+except ValueError:
+    print("Please enter a valid measurement")    
