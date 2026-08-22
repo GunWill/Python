c47a62f8b43fb05a33f9dec3569af32c3c6c9b82
GunWill <gunnar.willis@okstate.edu>
2026-08-22 12:44:53 -0500

    Update Lab1.py
    Lab Done

--- Lab1.py
+++ Lab1.py
@@ -1,4 +1,37 @@
+
 try:
-    measurement1 = float(input("Enter first Measurement in Centimeters"))
+    measurement1 = float(input("Enter first Measurement in Centimeters\n"))
 except ValueError:
-    print("Please enter a valid measurement")    
+    print("Invalid 1st measurement")
+
+
+try:
+    measurement2 = float(input("Enter second Measurement in Centimeters\n"))
+except ValueError:
+    print("Invalid 2nd measurement") 
+
+
+
+try:
+    measurement3 = float(input("Enter third Measurement in Centimeters\n"))
+except ValueError:
+    print("Invalid 3rd measurement")
+        
+
+
+try:
+    measurement4 = float(input("Enter fourth Measurement in Centimeters\n"))
+except ValueError:
+    print("Invalid 4th measurement") 
+      
+
+
+try:
+    measurement5 = float(input("Enter fifth Measurement in Centimeters\n"))
+except ValueError:
+    print("Invalid 5th measurement")  
+   
+print("The max value is", max(measurement1, measurement2, measurement3, measurement4, measurement5)) 
+print("The min value is", min(measurement1, measurement2, measurement3, measurement4, measurement5)) 
+measurements = [measurement1, measurement2, measurement3, measurement4, measurement5] #Put measurements in array to divide by a "dynamic" length, even though its always a length of 5
+print("The average value is", sum(measurements)/len(measurements)) 
